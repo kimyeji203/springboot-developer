8f4eae944d003b7c9b6eab5e1f71451fd8f09133
kimyeji203 <kimyeji203@naver.com>
2023-08-05 00:19:15 +0900

    20230805-스프링부트3백엔드개발자되기_2장

--- src/main/java/com/yjkim/springbootdeveloper/TestController.java
+++ src/main/java/com/yjkim/springbootdeveloper/TestController.java
@@ -1,8 +1,13 @@
 package com.yjkim.springbootdeveloper;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * {@link RestController}
+ *  {@link Controller}을 상속 => {@link org.springframework.stereotype.Component} 가지고 있음
+ */
 @RestController
 public class TestController
 {
